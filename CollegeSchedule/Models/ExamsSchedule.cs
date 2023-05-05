baef05807fd3907870003c39a342e6dd79eea08d
KarimBozzhanov <k.bozzhanov01.09.2006@gmail.com>
2023-05-05 13:59:31 +0600

    Last one

---
+++ CollegeSchedule/Models/ExamsSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CollegeSchedule.Models
+{
+    public class ExamsSchedule
+    {
+        public int Id { get; set; }
+        public int ExamNumber { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string ExamName { get; set; }
+        public string Room { get; set; }
+        [ForeignKey("TeacherId")]
+        public virtual Teacher Teacher { get; set; }
+        public int? TeacherId { get; set; }
+        [ForeignKey("GroupId")]
+        public virtual Group Group { get; set; }
+        public int? GroupId { get; set; }
+        
+    }
+}
